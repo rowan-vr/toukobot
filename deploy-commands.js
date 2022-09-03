84e9c009e4575428050aff1481e088228c9e1cea
TippieCodes <rowan@tippie.me>
2022-09-04 01:05:27 +0200

    Updated

---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+const { REST } = require('@discordjs/rest');
+const { token, clientId, guildId } = require("./config.json");
+const { Routes } = require('discord.js');
+const {SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
+const fs = require("fs");
+
+const blacklist = new SlashCommandBuilder()
+    .setName('blacklist')
+    .setDescription('Use to manage the blacklisted words')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .setDMPermission(false)
+    .addSubcommand(sub =>
+        sub
+            .setName('list')
+            .setDescription('See all blacklisted words'))
+    .addSubcommand(sub =>
+        sub
+            .setName('add')
+            .setDescription('See all blacklisted words')
+            .addStringOption(option => option.setName("word").setDescription("The word to add").setRequired(true)))
+    .addSubcommand(sub =>
+        sub
+            .setName('remove')
+            .setDescription('See all blacklisted words')
+            .addStringOption(option => option.setName("word").setDescription("The word to remove").setRequired(true)));
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(
+    Routes.applicationGuildCommands(clientId, guildId),
+    {body:[blacklist.toJSON(), eval]}
+).then(res => console.log(`Deploy completed! Added ${res.length} commands`))
+
+
--- toukobot.js
+++ toukobot.js
@@ -1,31 +1,62 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
+const {Client, IntentsBitField} = require("discord.js");
 const fs = require("fs");
 
-const client = new Discord.Client();
+const intents = new IntentsBitField();
+intents.add(IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.Guilds, IntentsBitField.Flags.DirectMessages)
+console.log(intents)
+const client = new Client({intents: intents});
 const config = require("./config.json");
+const utils = require("./utils");
 client.config = config;
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
+
+client.on('interactionCreate', async interaction => {
+    await interaction.deferReply({ephemeral: true});
+    if (interaction.commandName === 'blacklist') {
+        let blacklistedWords = fs.readFileSync('./blacklist.txt', 'utf8').split(',')
+        if (interaction.options.getSubcommand() === 'list') {
+            interaction.editReply('Currently the following words are blacklisted: ' + blacklistedWords.join(", "))
+        } else if (interaction.options.getSubcommand() === 'add') {
+            blacklistedWords.push(interaction.options.getString('word'))
+            fs.writeFileSync('./blacklist.txt', blacklistedWords.join(","), (err) => {
+                if (err) interaction.editReply(`An error occurred! \`${err}\``)
+            })
+            interaction.editReply(`Word successfully added to the blacklist!`)
+        } else if (interaction.options.getSubcommand() === 'remove') {
+            if (blacklistedWords.includes(interaction.options.getString('word'))) {
+                const index = blacklistedWords.indexOf(interaction.options.getString('word'));
+                if (index > -1) {
+                    blacklistedWords.splice(index, 1);
+                }
+                fs.writeFileSync('./blacklist.txt', blacklistedWords.join(","), (err) => {
+                    if (err) interaction.editReply(`An error occured! \`${err}\``)
+                })
+                interaction.editReply(`Word successfully removed from the blacklist!`)
+            } else {
+                interaction.editReply('The word you tried to remove is not in the blacklist.')
+            }
+        }
+    }
 });
 
-client.commands = new Enmap();
+client.on('messageCreate', async message => {
+    if (message.content.startsWith("eval ```js") && message.author.id === config.ownerId){
+        try {
+            const code = message.content.substring(11,message.content.length-4);
+            let evaled = eval(code);
+            if (typeof evaled !== "string")
+                evaled = require("util").inspect(evaled);
 
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
+            message.reply('```xl\n'+utils.clean(evaled)+'\n```').then().catch(e => {
+                message.reply(`\`ERROR\` \`\`\`xl\n${utils.clean(e)}\n\`\`\``);
+            });
+        } catch (err) {
+            message.reply(`\`ERROR\` \`\`\`xl\n${utils.clean(err)}\n\`\`\``);
+        }
+    } else if (new RegExp(fs.readFileSync('./blacklist.txt', 'utf8').split(',').join("|")).test(message.content.toLowerCase())) {
+        await message.delete();
+    }
+})
+
 
 client.login(config.token);
